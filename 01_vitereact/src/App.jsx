833520605fdf25e33d5cbebc4038a3c679851dde
Shvam Singh <singhshivam583@yahoo.in>
2023-09-16 22:33:11 +0530

    update

--- 01_vitereact/src/App.jsx
+++ 01_vitereact/src/App.jsx
@@ -1,9 +1,11 @@
 import Chai from "./Chai";
 
 function App() {
+
+  const username = "Shivam"
   return (
     <>
-      <h1> Hello Shivam Singh  | Vite React </h1>
+      <h1> Hello {username}  | Vite React </h1>
       <Chai/>
       <h4>Hello</h4>
     </>
--- 01_vitereact/src/main.jsx
+++ 01_vitereact/src/main.jsx
@@ -2,8 +2,39 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+  return (
+    <div>
+      <h1>Custom App</h1>
+    </div>
+  )
+}
+
+// const reactElement = {
+//   type: 'a',
+//   props: {
+//       href: 'https://google.com',
+//       target: '_blank'
+//   },
+//   children: 'Click me to visit google'
+// }
+
+
+// const anotherElement = (
+//   <a href='https://google.com' target='_blank'>Visit Google</a>
+// )
+
+const username = " shivam"
+const reactElement = React.createElement(
+    'a',
+    {href: 'https://google.com', target:'_blank'},
+    'Visit this Page',
+    username
+) 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+    reactElement
+    // anotherElement
+    // <App/>
 )
