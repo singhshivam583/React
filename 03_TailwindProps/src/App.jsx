222cc3523b23e7ca25bb969362abbf83893c8ac7
Shvam Singh <singhshivam583@yahoo.in>
2023-09-18 12:42:46 +0530

    update

--- 03_TailwindProps/src/App.jsx
+++ 03_TailwindProps/src/App.jsx
@@ -1,14 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  let myObj = {
+    username:"Shivam",
+    age: 21
+  }
+
+  let newArr = [1, 2, 3, 4]
+
   return (
     <>
-      <h1></h1>
+      <h1 className='bg-green-400 text-black p-4 rounded-xl'>Tailwind Test</h1>
+      <Card btnText = 'click me'/>
+      <Card username = 'Shivam' />
     </>
   )
 }
