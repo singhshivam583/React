18e7c8f3a558a8cfd4f1ddcf806427a13377a7f7
Shvam Singh <singhshivam583@yahoo.in>
2023-09-23 23:20:19 +0530

    update

--- 12_MegaBlog/src/App.jsx
+++ 12_MegaBlog/src/App.jsx
@@ -2,8 +2,7 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  console.log(import.meta.env.VITE_APPWRITE_URL);
   return (
     <>
       <h1 className='text-red-500'>Shivam</h1>
