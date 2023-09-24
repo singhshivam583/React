4952ce69409b21b38255ebb00aa6810fb5836715
Shvam Singh <singhshivam583@yahoo.in>
2023-09-24 22:47:18 +0530

    update

--- 12_MegaBlog/src/appwrite/auth.js
+++ 12_MegaBlog/src/appwrite/auth.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-useless-catch */
 import config from "../config/config";
 import { Account, Client, Databases, ID } from "appwrite";
 
@@ -36,12 +37,12 @@ export class AuthService{
     }
 
     async  createAccount({email, password, name}){
-        // eslint-disable-next-line no-useless-catch
         try {
             const userAccount = await this.account.create(
                 ID.unique(), email, password, name); 
             if (userAccount) {
                 // call another method
+                return this.login({email, password});
                 
             }else {
                 return userAccount;
@@ -49,6 +50,33 @@ export class AuthService{
 
         } catch (error) {
             throw error; 
+        }
+    }
+
+    async login({email, password}){
+        try {
+           return await this.account.createEmailSession(email, password);
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async getCurrentUser(){
+        try {
+            return await this.account.get();
+
+        } catch (error) {
+            console.log("Appwrite service :: getCurrentUser :: error", error);
+        }
+
+        return null;
+    }
+
+    async logout(){
+        try {
+            await this.account.deleteSessions();
+        } catch (error) {
+            console.log("Appwrite Service :: logout :: error", error);
         }
     }
 }
