a1b1ad1b3045e34cdcf4aab5625fa27a6d6c5c2b
Shvam Singh <singhshivam583@yahoo.in>
2023-09-26 07:40:47 +0530

    update

--- 12_MegaBlog/src/appwrite/config.js
+++ 12_MegaBlog/src/appwrite/config.js
@@ -56,6 +56,91 @@ export class Services{
             );
         }
     }
+
+    async deletePost(slug){
+        try {
+             await this.databases.deleteDocument(
+                conf.appwriteDatabaseId, 
+                conf.appwriteCollectionId, 
+                slug, 
+            )
+            return true;
+
+        } catch (error) {
+            console.log(
+                `Appwrite service :: deletePost :: error`,error
+            );
+        }
+    }
+
+    async getPost(slug){
+        try {
+            return await this.databases.deleteDocument(
+                conf.appwriteDatabaseId, 
+                conf.appwriteCollectionId, 
+                slug, 
+            )
+        } catch (error) {
+            console.log(
+                `Appwrite service :: getPost :: error`,error
+            );
+            return false;
+        }
+    }
+
+    async getPosts(queries = [Query.equal("status", "active")]){
+        try {
+            return await this.databases.listDocuments(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                queries,
+            )
+        } catch (error) {
+            console.log(
+                `Appwrite service :: getPosts :: error`,error
+            );
+            return false;
+        }
+        return false;
+    }
+
+    // File related services 
+    async fileUpload(file){
+        try {
+            return await this.bucket.createFile(
+                conf.appwriteBucketId,
+                ID.unique(),
+                file
+            )
+        } catch (error) {
+            console.log(
+                `Appwrite service :: fileUpload :: error`,error
+            );
+            return false;
+        }
+    }
+
+    async deleteFile(fileId){
+        try {
+            await this.bucket.deleteFile(
+                conf.appwriteBucketId,
+                fileId
+            )
+            return true;
+        } catch (error) {
+            console.log(
+                `Appwrite service :: deleteFile :: error`,error
+            );
+            return false;
+        }
+    }
+
+    getFilePreview(fileId){
+        return this.bucket.getFilePreview(
+            conf.appwriteBucketId,
+            fileId
+        )
+    }
 }
 
 const services = new Services();
