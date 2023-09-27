84bff5d974995defe3f35be8a916e0a2a620a645
Shvam Singh <singhshivam583@yahoo.in>
2023-09-27 10:11:12 +0530

    update

--- 12_MegaBlog/src/App.jsx
+++ 12_MegaBlog/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import {useDispatch} from 'react-redux'
 import authService from './appwrite/auth'
 import {login, logout} from './store/authSlice'
@@ -23,17 +23,24 @@ function App() {
 
   }, [])
 
-  return  !loading? (
+  // return(
+  //         <>
+  //            <div>
+  //                <h1>Hello</h1>
+  //            </div>
+  //         </>
+  // )
+
+  return (!loading ? (
     <div className="min-h-screen flex flex-wrap content-between bg-gray-400 ">
       <div className='w-full block'>
           <Header/>
           <main>
-            Todo{/* {Outlet} */}
           </main>
           <Footer/>
       </div>
     </div>
-  ) : null
+  ) : null)
 }
 
 export default App
