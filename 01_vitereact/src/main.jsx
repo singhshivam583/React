e7b42a173f664ebc6c70ec5624a0ad70ce1e1bff
Shvam Singh <singhshivam583@yahoo.in>
2024-02-10 15:45:41 +0530

    basic changes

--- 01_vitereact/src/main.jsx
+++ 01_vitereact/src/main.jsx
@@ -1,14 +1,14 @@
-// import React from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-// function MyApp(){
-//   return (
-//     <div>
-//       <h1>Custom App</h1>
-//     </div>
-//   )
-// }
+function MyApp(){
+  return (
+    <div>
+      <h1>Custom App</h1>
+    </div>
+  )
+}
 
 // const reactElement = {
 //   type: 'a',
@@ -20,21 +20,22 @@ import App from './App.jsx'
 // }
 
 
-// const anotherElement = (
-//   <a href='https://google.com' target='_blank'>Visit Google</a>
-// )
+const anotherElement = (
+  <a href='https://google.com' target='_blank'>Visit Google</a>
+)
 
-// const username = " shivam"
-// const reactElement = React.createElement(
-//     'a',                                              // at first always a tag is there 
-//     {href: 'https://google.com', target:'_blank'},    // at second always a object is there which contains all the properties or arguments of that element
-//     'Visit this Page',                                // at third always  the text is there
-//     username                                          // rest are variables that are evaluated expressions 
-// ) 
+const username = " shivam"
+const reactElement = React.createElement(
+    'a',                                              // at first always a tag is there 
+    {href: 'https://google.com', target:'_blank'},    // at second always a object is there which contains all the properties or arguments of that element
+    'Visit this Page',                                // at third always  the text is there
+    username                                          // rest are variables that are evaluated expressions 
+) 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-
+    
     // reactElement
     // anotherElement
+    // <MyApp/>
     <App/>
 )
