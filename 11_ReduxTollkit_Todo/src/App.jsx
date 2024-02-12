0034dc72515794a81897b4b6941104d67bcf2a43
Shvam Singh <singhshivam583@yahoo.in>
2024-02-13 01:29:32 +0530

    added update todo redux function

--- 11_ReduxTollkit_Todo/src/App.jsx
+++ 11_ReduxTollkit_Todo/src/App.jsx
@@ -1,14 +1,26 @@
+import { useEffect } from 'react'
 import './App.css'
 import AddTodo from './components/AddTodo'
+import { useSelector,} from 'react-redux'
 import Todos from './components/Todos'
+
+
 
 function App() {
 
+  const todos = useSelector(state => state.todos)
+
   return (
     <>
-      <h1 className='text-white'>Learn About Redux Toolkit</h1>
-      <AddTodo />
-      <Todos />
+      <div className='bg-slate-400 p-4 items-center rounded-lg mb-4'>
+        <h1 className='text-black text-xl font-bold'>Learn About Redux Toolkit</h1>
+        <AddTodo />
+        <ul className='list-none' >
+          {todos.map((todo) => (
+            <Todos key={todo.id} todo = {todo}/>
+          ))}
+        </ul>
+      </div>
     </>
   )
 }
--- 11_ReduxTollkit_Todo/src/features/todo/todoSlice.js
+++ 11_ReduxTollkit_Todo/src/features/todo/todoSlice.js
@@ -5,24 +5,36 @@ const initialState = {
 }
 
 
-
 export const todoSlice = createSlice({
     name: 'todo',
     initialState,
     reducers: {
-        addTodo: (state, action) => {
+        addTodo: (state, action) => {              // reducer functions always receive two  arguments: the current state and an action
             const todo = {
                 id: nanoid(), 
-                text: action.payload
+                text: action.payload              // payload is a object that receive a value
             }
+            console.log(todo)
             state.todos.push(todo)
         },
+
         removeTodo: (state, action) => {
             state.todos = state.todos.filter((todo) => todo.id !== action.payload )
+        },
+
+        updateTodo:(state, action)=>{
+            console.log(action.payload);
+            state.todos = state.todos.map((eachVal) => ( eachVal.id === action.payload.id ? action.payload : eachVal));
+
+            // another method finding index and updating the arrays
+            // let index=state.todos.findIndex((item)=> item.id===action.payload.id);
+            // if(index!==-1){
+            //    state.todos[index] = action.payload;
+            // }  
         },
     }
 })
 
-export const {addTodo, removeTodo} = todoSlice.actions
+export const {addTodo, removeTodo, updateTodo} = todoSlice.actions
 
 export default todoSlice.reducer
